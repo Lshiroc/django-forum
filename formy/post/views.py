56f3cbde8ff887ce1b86574b00702d6815fdda15
Lshiroc <zeynalmerdanli4321@gmail.com>
2024-02-14 22:40:30 +0400

    fixed post not saving issue

--- formy/post/views.py
+++ formy/post/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 import json
 
 from .forms import NewPostForm
 from .models import Tag
 
+@login_required
 def new(request):
     if request.method == 'POST':
         form = NewPostForm(request.POST)
-        if form.is_valid():
-            form.posted_by = request.user
-            tags = json.loads(request.POST['tags'])['tags']
-            tagsModels = []
-            for tag in tags:
-                tag = Tag.objects.get_or_create(name=tag)
-                tagsModels.append(tag)
+        form.instance.posted_by = request.user
+        tags = json.loads(request.POST['tags'])['tags']
+        tagsModels = []
+        for tag in tags:
+            tag = Tag.objects.get_or_create(name=tag)
+            tagsModels.append(tag)
+        form.tags = tagsModels
 
-            form.tags = tagsModels
+        if form.is_valid():
             form.save()
             return redirect('/')
     else:
