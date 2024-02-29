0e77440478f03ccc046b8feb915a1c5b193573ad
Lshiroc <zeynalmerdanli4321@gmail.com>
2024-03-01 00:14:10 +0400

    trying to manually add the cropped image to imageField

--- formy/accounts/views.py
+++ formy/accounts/views.py
@@ -8,14 +8,21 @@ from .forms import UserProfileChange
 @login_required
 def profile(request):
     if request.method == "POST":
-        form = UserProfileChange(request.POST, request.FILES)
+        form = UserProfileChange(request.POST)
+        print(request.FILES['profile_picture_full'])
+
+        data = request.POST['crop_details']
+        cropDetails = json.loads(data)
+        # print(img)
+        print(request.FILES['profile_picture_full'])
+        img = Image.open(request.FILES['profile_picture_full'])
+        width, height = img.size
+        print(cropDetails)
+        img.crop((cropDetails['x'], cropDetails['y'], width*cropDetails['scale'], height*cropDetails['scale']))
+        form.instance.profile_picture.save(request.FILES['profile_picture_full'], img.read(), True)
+       
         if form.is_valid():
-            data = request.POST['crop_details']
-            cropDetails = json.loads(data)
-            img = Image.open(request.FILES['profile_picture_full'])
-            width, height = im.size
-            img.crop((cropDetails.x, cropDetails.y, width*cropDetails.scale, height*cropDetails.scale))
-            print(cropDetails)
+            print("valid")
             
             # form.save()
         else:
